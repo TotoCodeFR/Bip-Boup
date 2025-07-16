c710aff937b9ff9fbe88f5ef27a2c687aa68a157
TotoCode <totocodefr@duck.com>
2025-07-16 09:01:42 +0000

    fix tdb ne charge pas pdp et nom d'utilisateur

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production', // HTTPS en prod
+    secure: false,
     maxAge: 1000 * 60 * 60 * 24 * 7 // 7 jours
   }
 }));
